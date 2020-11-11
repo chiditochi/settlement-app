af14a20a54cd9d4316c568018ed74743dc643b6c
Ukaigwe Chidi <chiditochi@yahoo.com>
2020-11-11 17:09:02 +0100

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace settlement_processing_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
